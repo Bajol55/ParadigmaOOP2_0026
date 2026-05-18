0aac4a98b0d7df87d5a7be841eab7f35da651e98
Bajol55 <devarafadil35@gmail.com>
2026-05-18 12:11:27 +0700

    menambahkan namespace

---
+++ abstraksi.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+class AbstraksiKls{
+    private: string x, y;
+    public:
+
+    //
+    //
+    void setXY(string a, string b){
+        x = a;
+    }     y = b;
+    //
+    void display(){
+        cout << "Nilai x: " << x << endl;
+        cout << "Nilai y: " << y << endl;
+    }
+};
+
+int main(){
+    AbstraksiKls ak;
+    ak.setXY("Yogyakarta", "kampus");
+    ak.display();
+
+    return 0;
+}
